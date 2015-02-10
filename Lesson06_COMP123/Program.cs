d560da240fc0c5858a12825f5a91c0c0352b2448
JihoYoo <davidyoo214@gmail.com>
2015-02-10 12:30:54 -0500

    Create Project

---
+++ Lesson06_COMP123/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson06_COMP123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
