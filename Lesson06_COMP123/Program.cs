a6875766b65c2e2635db0a8bec5d14ca237d73b9
JihoYoo <davidyoo214@gmail.com>
2015-02-10 13:18:51 -0500

    Get,,get

--- Lesson06_COMP123/Program.cs
+++ Lesson06_COMP123/Program.cs
@@ -10,7 +10,21 @@ namespace Lesson06_COMP123
     {
         static void Main(string[] args)
         {
+            Class1 myClass = new Class1("Hello");
 
+            myClass.IntergerNumber = 10;
+            myClass.DoubleNumber = 50.0;
+
+
+            myClass.show();
+
+            Console.Write("\n");
+
+            Console.WriteLine("++++++++++++++++++++++++++");
+
+            Console.WriteLine("Press hit any key to exit");
+
+            Console.ReadKey();
         }
     }
 }
